4f7e35e11666b7205d3f997c03c0f9c8623052fb
nmthong226 <nmthong226@gmail.com>
2025-02-03 23:51:01 +0700

    UI updates

---
+++ src/components/slotMachine/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from "react";
+import "./style.scss";
+
+// const iconMap = ["Tôm", "Cá", "Cua", "Gà", "Nai", "Bầu"];
+const iconHeight = 79;
+const numIcons = 6;
+const timePerIcon = 100;
+
+const SlotMachine: React.FC = () => {
+  const [indexes, setIndexes] = useState<number[]>([0, 0, 0]);
+  const [rolling, setRolling] = useState<boolean>(false);
+  const reelsRef = useRef<HTMLDivElement[]>([]);
+
+  const roll = (reel: HTMLDivElement, offset: number): Promise<number> => {
+    const delta = (offset + 2) * numIcons + Math.floor(Math.random() * numIcons);
+    const backgroundPositionY = parseFloat(getComputedStyle(reel).backgroundPositionY) || 0;
+    const targetBackgroundPositionY = backgroundPositionY + delta * iconHeight;
+    const normTargetBackgroundPositionY = targetBackgroundPositionY % (numIcons * iconHeight);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        reel.style.transition = `background-position-y ${(8 + delta) * timePerIcon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+        reel.style.backgroundPositionY = `${targetBackgroundPositionY}px`;
+      }, offset * 150);
+
+      setTimeout(() => {
+        reel.style.transition = "none";
+        reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+        resolve(delta % numIcons);
+      }, (8 + delta) * timePerIcon + offset * 150);
+    });
+  };
+
+  const rollAll = () => {
+    setRolling(true);
+    if (reelsRef.current.length !== 3) return;
+
+    Promise.all(reelsRef.current.map((reel, i) => roll(reel, i))).then((deltas) => {
+      setIndexes((prevIndexes) =>
+        prevIndexes.map((index, i) => (index + deltas[i]) % numIcons)
+      );
+      setRolling(false);
+    });
+  };
+
+  return (
+    <div>
+      <button
+        className={`-top-1 right-4 absolute flex items-center rounded-md -translate-y-1/2 ${rolling ? 'brightness-75 cursor-not-allowed' : 'hover:brightness-105 hover:cursor-pointer'}`}
+        disabled={rolling}
+        onClick={rollAll}>
+        <img src='/but_3.png' className="w-32" />
+      </button>
+      <div className="scale-110 slot-machine">
+        <div className="space-x-1 rounded-md slots">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <div
+              key={i}
+              className="reel"
+              ref={(el) => el && (reelsRef.current[i] = el)}
+              style={{ backgroundPositionY: `${indexes[i] * iconHeight}px` }}
+            />
+          ))}
+        </div>
+        {/* <div className="">{rolling ? "Chờ Tí..." : indexes.map((i) => iconMap[i]).join(" - ")}</div> */}
+      </div>
+    </div>
+  );
+};
+
+export default SlotMachine;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,168 @@
+import { RiMoneyEuroBoxLine } from "react-icons/ri";
+import { CiBoxList } from "react-icons/ci";
+import SlotMachine from "../../components/slotMachine";
+
+interface Player {
+  id: number;
+  name: string;
+  award: number;
+}
+
+const players: Player[] = [
+  { id: 1, name: "Công Hậu", award: 12000 },
+  { id: 2, name: "Hoàng Linh", award: 8000 },
+  { id: 3, name: "Phương Toàn", award: 8000 },
+  { id: 4, name: "Dịu Hiền", award: 6000 },
+  { id: 5, name: "Đức Anh", award: 4000 },
+  { id: 6, name: "Thành Nhân", award: 2000 },
+  { id: 7, name: "Trọng Huy", award: 2000 },
+  { id: 8, name: "Đại Vĩ", award: 0 },
+];
+
 const Home = () => {
   return (
     <div className="flex items-end gap-2 rounded-md w-full h-full">
-      <div className="relative flex flex-col border-10 border-white shadow-xl rounded-md w-3/4 h-[98%]">
+      <div className="relative flex flex-col border-10 border-white shadow-xl rounded-md w-[80%] h-[98%]">
         <div className="-top-1 left-4 absolute flex items-center rounded-md -translate-y-1/2">
-          <img src='/name_2.png' className="w-68" />
+          <img src='/name_2.png' className="hover:brightness-105 w-68 hover:cursor-pointer" />
         </div>
-        <div className="-top-1 right-4 absolute flex items-center rounded-md -translate-y-1/2">
-          <img src='/but_3.png' className="w-32" />
-        </div>
-        <div className="flex border-[1px] bg-[#fff8e2] border-black rounded-md w-full h-full">
-
+        <div className="flex flex-col border-[#703626] border-[2px] bg-[#fff8e2] rounded-md w-full h-full">
+          <div className="relative flex flex-col justify-center items-center h-[60%]">
+            <SlotMachine />
+          </div>
+          <div className="relative flex justify-between items-center p-2 w-full h-[40%]">
+            <img src="/nuatre_trai_2.png" className="w-full h-full" />
+            <img src="/nuatre_2.png" className="w-full h-full" />
+            <div className="absolute flex justify-center items-center space-x-10 mt-5 w-full h-full">
+              <div className="flex flex-col justify-center items-center">
+                <img src='/dat_tom.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center w-full h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+              <div className="flex flex-col justify-center">
+                <img src='/dat_bau.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+              <div className="flex flex-col justify-center">
+                <img src='/dat_ga.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <img src='/dat_ca.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+              <div className="flex flex-col justify-center">
+                <img src='/dat_cua.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+              <div className="flex flex-col justify-center">
+                <img src='/dat_nai.png' className="w-full h-38" />
+                {/* <button
+                  className="relative hover:brightness-105 flex justify-center items-center h-16 hover:cursor-pointer"
+                >
+                  <img src='/button_type2.png' className="absolute w-full h-full" />
+                  <div className="z-10 flex flex-col justify-center items-center font-bold text-amber-800 capitalize">
+                    <p>Dịu Hiền</p>
+                  </div>
+                </button> */}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col justify-end rounded-md w-1/4 h-full">
-        <div className="relative flex border-10 border-white bg-[#fff8e2] shadow-xl rounded-md w-full h-[42%]">
+      <div className="flex flex-col justify-end rounded-md w-[20%] h-full">
+        <div className="relative flex border-10 border-white bg-[#fff8e2] shadow-xl rounded-md w-full h-[44%]">
           <div className="-top-1 left-1/2 absolute flex items-center rounded-md w-32 h-8 -translate-x-1/2 -translate-y-1/2">
             <img src='/but_1.png' />
           </div>
-          <div className="flex border-[1px] bg-[#fff8e2] border-black rounded-md w-full h-full">
+          <div className="flex flex-col space-y-4 border-[#703626] border-[2px] bg-[#fff8e2] p-2 rounded-md w-full h-full">
+            <div className="flex bg-[#ffdf8d] mt-5 rounded-xl w-full h-24">
+              <div className="flex flex-col justify-center items-center space-y-1 w-2/3 h-full">
+                <p className="font-bold text-sm">MÃ PHÒNG</p>
+                <p className="bg-[#ffcb46] px-6 py-1 border rounded-xl font-bold text-2xl">092317</p>
+              </div>
+              <div className="flex justify-center items-center w-1/3 h-full">
+                <img src='/sample_qr.png' className="bg-gray-50 rounded-md w-20 h-20" />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center space-y-2 bg-[#ffdf8d] p-2 rounded-xl w-full h-40">
+              <div className="flex justify-between items-center pl-4 w-full font-bold">
+                <p className="w-1/3">HOST</p>
+                <p className="flex justify-end items-center bg-[#ffcb46] p-2 border rounded-lg w-2/3 capitalize">
+                  Minh Thông
+                </p>
+              </div>
+              <div className="flex justify-between items-center pl-4 w-full font-bold">
+                <p className="w-1/3">$$$</p>
+                <p className="flex justify-end items-center bg-[#ffcb46] p-2 border rounded-lg w-2/3 capitalize">
+                  20000
+                </p>
+              </div>
+              <div className="flex justify-between items-center space-x-3 w-full">
+                <button className="flex justify-center items-center bg-[#ffffb7] border rounded-lg w-1/3 h-8 text-nowrap text-xs">
+                  <RiMoneyEuroBoxLine className="mr-1" />
+                  <p className="font-semibold">Tính tiền</p>
+                </button>
+                <button className="flex justify-center items-center bg-[#ffffb7] border rounded-lg w-2/3 h-8 text-xs">
+                  <CiBoxList className="mr-1" />
+                  <p className="font-semibold">Người chơi</p>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="relative flex border-10 border-white bg-[#fff8e2] shadow-xl mt-11 rounded-md w-full h-[50%]">
+        <div className="relative flex border-10 border-white bg-[#fff8e2] shadow-xl mt-11 rounded-md w-full h-[48%]">
           <div className="-top-1 left-1/2 absolute flex items-center rounded-md w-32 h-8 -translate-x-1/2 -translate-y-1/2">
             <img src='/but_2.png' />
           </div>
-          <div className="flex border-[1px] bg-[#fff8e2] border-black rounded-md w-full h-full">
-
+          <div className="flex border-[2px] border-[rgb(112,54,38)] bg-[#fff8e2] rounded-md w-full h-full">
+            <div className="grid grid-rows-1 w-full overflow-hidden">
+              <table className="border-collapse mt-5 w-full">
+                <tbody>
+                  {players.map((player) => (
+                    <tr key={player.id} className={`odd:bg-[#ffdf8d] text-sm font-semibold ${player.id === 1 ? '' : 'border-t'}`}>
+                      <td className="px-2 py-1">{player.id}</td>
+                      <td className="px-2 py-1">{player.name}</td>
+                      <td className="text-right px-2 py-1">{player.award}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
